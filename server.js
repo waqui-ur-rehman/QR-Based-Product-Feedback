d664052308be5e4bba1db2341e6e7cd1d85df390
Syed M Waqui-Ur-Reman <rehmansyed493@gmail.com>
2026-08-24 05:11:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -65,8 +65,6 @@ mongoose.connect(process.env.MONGO_URI)
     });
 
 
-app.listen(3000, function () {
-
-    console.log("Server is running on port 3000");
-
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running");
 });

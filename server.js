04f52c1475c632741d363de5b98ab4f193248754
Syed M Waqui-Ur-Reman <rehmansyed493@gmail.com>
2026-08-24 05:59:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,70 +1,27 @@
 var express = require("express");
 var mongoose = require("mongoose");
-require("dotenv").config();
+var dotenv = require("dotenv");
+
+dotenv.config();
 
 var app = express();
 
 app.use(express.json());
+
 app.use(express.static("public"));
 
-
-// Review ka structure
-var reviewSchema = new mongoose.Schema({
-
-    name: String,
-    rating: Number,
-    review: String
-
-});
-
-
-// MongoDB mein Review collection
-var Review = mongoose.model("Review", reviewSchema);
-
-
-// Sab reviews get karna
-app.get("/reviews", async function (req, res) {
-
-    var reviews = await Review.find();
-
-    res.json(reviews);
-
-});
-
-
-// New review add karna
-app.post("/reviews", async function (req, res) {
-
-    var newReview = new Review({
-
-        name: req.body.name,
-        rating: req.body.rating,
-        review: req.body.review
-
-    });
-
-    await newReview.save();
-
-    res.json(newReview);
-
-});
-
-
 mongoose.connect(process.env.MONGO_URI)
-
     .then(function () {
-
         console.log("MongoDB connected successfully!");
-
     })
-
     .catch(function (error) {
-
         console.log("MongoDB connection error:", error);
-
     });
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
     console.log("Server is running");
 });

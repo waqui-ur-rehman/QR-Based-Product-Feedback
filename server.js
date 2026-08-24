da16777ab431d1e0d0f4983e5b9700347872a353
Syed M Waqui-Ur-Reman <rehmansyed493@gmail.com>
2026-08-24 06:17:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,29 +9,28 @@ var app = express();
 app.use(express.json());
 
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 
 app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
-    res.sendFile(__dirname + "/index.html");
 
+app.get("/test", function (req, res) {
+    res.send("QR Feedback Server is working");
 });
 
 
 mongoose.connect(process.env.MONGO_URI)
-
     .then(function () {
-       console.log("MongoDB connected successfully!");
-
+        console.log("MongoDB connected successfully!");
     })
     .catch(function (error) {
         console.log("MongoDB connection error:", error);
-
     });
 
+
 app.listen(process.env.PORT || 3000, "0.0.0.0", function () {
-
     console.log("Server is running");
-
 });
